4e1d130eb9263adcfa54876871a7ccb3f9371337
Luís Otávio Azevedo Maciel <luis.maciel@cwi.com.br>
2023-02-25 11:23:31 -0300

    🔧 Include ESLint + Prettier configs

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -3,49 +3,49 @@ import styled from 'styled-components/native';
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
-  alignItems: center;
-  justifyContent: center;
-  backgroundColor: #000000;
+  align-items: center;
+  justify-content: center;
+  background-color: #000000;
 `;
 
 export const LogoContainer = styled.View`
   flex: 1;
-  alignItems: center;
-  justifyContent: center;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const AnimatedLoginContainer = styled(Animated.View)`
   flex: 1;
-  alignItems: center;
+  align-items: center;
   width: 90%;
 `;
 
 export const Input = styled.TextInput`
-  backgroundColor: #e5e4e2;
+  background-color: #e5e4e2;
   width: 90%;
-  marginBottom: 15px;
+  margin-bottom: 15px;
   color: #222;
-  fontSize: 17px;
-  borderRadius: 7px;
+  font-size: 17px;
+  border-radius: 7px;
   padding: 10px;
 `;
 
 export const LoginButton = styled.TouchableOpacity`
-  backgroundColor: #f4e300;
+  background-color: #f4e300;
   width: 90%;
   height: 45px;
-  alignItems: center;
-  justifyContent: center;
-  borderradius: 7px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 7px;
 `;
 
 export const TextLoginButton = styled.Text`
   color: #000000;
-  fontSize: 18px;
+  font-size: 18px;
 `;
 
 export const RegisterButton = styled.TouchableOpacity`
-  marginTop: 10px;
+  margintop: 10px;
 `;
 
 export const TextRegisterButton = styled.Text`
